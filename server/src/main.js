776f12da9b912ce203790c3755fc422924a72cd7
Erregea5 <blastbaja15@gmail.com>
2024-02-24 18:04:47 -0600

    setup project and put some mini tutorials

---
+++ server/src/main.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const server = http.createServer();
+
+server.on('request', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify({
+    data: 'Hello World!',
+  }));
+});
+
+server.listen(8000);
