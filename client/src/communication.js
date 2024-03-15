81f8f56a2487dc92c7cdd9498f213f30366a45ad
tam1444AH <mohammedtalha290@gmail.com>
2024-03-14 19:00:52 -0500

    Trying to fill in communication.js to recieve data

--- client/src/communication.js
+++ client/src/communication.js
@@ -1,12 +1,28 @@
+import DisplayTable from './DisplayTable';
+
 function requestData(){
   fetch('url')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
     .catch(err=>{
-
-    }).then(data=>{
-
+      console.error('Error fetching data:', err);
+    })
+    .then(data=>{
+      renderTable(data);
     });
+
 }
 
 function postData(){
 
+}
+
+function renderTable(data) {
+  // Assuming you have a root element with id 'root' to render the DisplayTable component
+  const rootElement = document.getElementById('root');
+  ReactDOM.render(<DisplayTable data={data} />, rootElement);
 }
