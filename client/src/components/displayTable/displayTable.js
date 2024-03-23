57ec1a02d77b53a37341240017738bae1b0d9300
tam1444AH <145513739+tam1444AH@users.noreply.github.com>
2024-03-23 00:35:27 -0500

    Adding CRUD Buttons work in progress change if needed

--- client/src/components/displayTable/displayTable.js
+++ client/src/components/displayTable/displayTable.js
@@ -1,6 +1,8 @@
 import './table.css'
 import { postData,getData,updateData,deleteData } from '../../communication';
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import DataEntryPage from '../dataEntry/dataEntry';
 
 
 
@@ -14,12 +16,41 @@ function DisplayTable({link}){
     .catch(err => console.log(err))
   })
 
+  // Function to handle modification of data
+  const handleModify = (data, id) => {
+    // Your modification logic goes here
+    updateData("/animals", id, data) 
+      .then(res => {
+        // Handle success if needed
+        console.log('Data updated successfully:', res);
+      })
+      .catch(err => {
+        // Handle error if needed
+        console.error('Error updating data:', err);
+      });
+    console.log('Modify', id);
+  };
+
+  // Function to handle deletion of data
+  const handleDelete = (id) => {
+    // Delete data from the backend
+    deleteData('/animals', id)
+      .then(() => {
+        // Update state to reflect the deletion
+        setData(prevData => prevData.filter(item => item.Animal_ID !== id));
+      })
+      .catch(err => console.log(err));
+  };
+
   const dataColumns=[];
   for(let prop in data[0])
     dataColumns.push(prop);
 
   //data.sort((a,b)=>a.date-b.date)
   return <center>
+  <h1>Animal Data</h1>
+  {/* <Link class="Create" to="/dataEntry">Create +</Link> */}
+  <a href="#/dataEntry"><button>Create +</button></a>
   <section id="outer-table-container">
     <div id="inner-table-container">
       <table id='quote-table'>
@@ -36,8 +67,8 @@ function DisplayTable({link}){
                   
                 </td>)
             }
-            <button onClick={Modify}>Modify</button>
-            <button onClick={Delete}>Delete</button>
+            <button onClick={handleModify(data[idx], data[idx].Animal_ID)}>Modify</button>
+            <button onClick={handleDelete(data[idx].Animal_ID)}>Delete</button>
             </tr>
           </>)
         }</tbody>
@@ -47,12 +78,5 @@ function DisplayTable({link}){
   </center>
 }
 
-function Modify() {
-  
-}
-
-function Delete() {
-  console.log("Hello World");
-}
 
 export default DisplayTable;
