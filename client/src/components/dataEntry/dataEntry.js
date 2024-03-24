4c7c0e3afa1c604b1822383711ba456a0696df12
tam1444AH <145513739+tam1444AH@users.noreply.github.com>
2024-03-23 19:15:12 -0500

    Trying to finish the CRUD operations

--- client/src/components/dataEntry/dataEntry.js
+++ client/src/components/dataEntry/dataEntry.js
@@ -11,33 +11,6 @@ function DataEntryBanner() {
     );
 }
 
-// DisplayTable();
-
-// function Table({ data }) {
-//   if (!data || data.length === 0) {
-//       return <div>No data available</div>;
-//   }
-
-//   const headers = Object.keys(data[0]);
-
-//   return (
-//       <table>
-//           <thead>
-//               <tr>
-//                   {headers.map(header => <th key={header}>{header}</th>)}
-//               </tr>
-//           </thead>
-//           <tbody>
-//               {data.map((row, index) => (
-//                   <tr key={index}>
-//                       {headers.map(header => <td key={header}>{row[header]}</td>)}
-//                   </tr>
-//               ))}
-//           </tbody>
-//       </table>
-//   );
-// }
-
 const map={
   animal:{
     Name: {type:"text",text:"Animal Name:"},
