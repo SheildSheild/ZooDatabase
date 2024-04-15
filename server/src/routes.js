61301ae0662851bdb0b7fafb2e3c3868659fa41b
Sebastian De La Espriella <blastbaja15@gmail.com>
2024-04-15 00:22:07 +0000

    Merge branch 'main' of https://github.com/SheildSheild/ZooDatabase

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,4 +1,4 @@
-const m=['Manager'], em=['Medic',...m],e=['Employee',...em], c=['Customer'], cm=[...c,...m], a='All', n=[];
+const m=['Manager'], em=['Medic',...m],e=['Employee',...em], c=['Customer'], cm=[...c,...m], ce=[...c,...e], a='All', n=[];
 function access(v,m,c){
   return {
     view:v,
@@ -16,20 +16,20 @@ module.exports={
   routes:{
     "zones":access(e,m),
     "habitats":access(e,m),
-    "animals":access(a,e),
+    "animals":access(a,m),
     "shops":access(a,m),
     "restaurants":access(a,m),
     "employees":access(m,m),
     "animal_health":access(em,em),
-    "items":access(e,m),
+    "items":access(e,m,cm),
     "customers":access(cm,a),
     "lost_items":access(e,e),
     "complaints":access(m,c),
     "schedule_types":access(m,m),
     "employee_schedules":access(e,m),
-    "animal_schedules":access(e,m),
-    "shop_schedules":access(e,m),
-    "habitat_schedules":access(e,m),
+    "animal_schedules":access(ce,m),
+    "shop_schedules":access(ce,m),
+    "habitat_schedules":access(ce,m),
     "timesheets":access(e,m),
     "attends_to":access(e,m),
     "purchases":access(cm,m),
@@ -42,5 +42,6 @@ module.exports={
     "monthly_visit":access(m,n),
     "animals_view":access(m,n),
     "animal_health_view":access(m,n),
+    "alerts":access(m,n)
   }
 }
