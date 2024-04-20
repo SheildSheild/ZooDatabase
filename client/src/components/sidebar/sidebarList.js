6cb148742d89bd54d549c05bf4f8f77131ded665
tam1444AH <145513739+tam1444AH@users.noreply.github.com>
2024-04-20 15:35:05 -0500

    Profit report deleted as request of No

--- client/src/components/sidebar/sidebarList.js
+++ client/src/components/sidebar/sidebarList.js
@@ -113,9 +113,6 @@ const managerLinks = [
   { group: 'Reports', text: 'View Zoo Visit Report', onClick: (userData,setMainComponent)=>{
     setMainComponent(<Report route='/visit_count' title='Report Number Of Visitor'/>)
   } },
-  { group: 'Reports', text: 'View Profit Report', onClick: (userData,setMainComponent)=>{
-    setMainComponent(<Report route='/profit' title='Report Profit'/>)
-  } },
 ];
 
 export {customerLinks,employeeLinks,medicLinks,managerLinks};
